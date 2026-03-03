6efe7d3034e9632e233a15fcba2896ce060b9c6c
Sanchitosss <Sashashurnov.777@gmail.com>
2026-03-03 21:06:27 +0300

    Update database.py

--- Алекс Банк/backend/database/database.py
+++ Алекс Банк/backend/database/database.py
@@ -8,60 +8,42 @@ class DataBase:
     def __init__(self, file_db):
         self.file_db = file_db
 
+
     # проверка на наличие пользователя
     def user_presence(self, number_phone, password):
         try:
             with sqlite3.connect(self.file_db) as conn:
                 cursor = conn.cursor()
                 cursor.execute(USER_PRESENCE, (number_phone, password))
-                user = cursor.fetchone()
-                if user:
-                    return True, "Пользователь найден"
-                return False, "Пользователь не найден"
+                data_user = cursor.fetchone()
+                if data_user:
+                    return True, "Пользователь найден", data_user
+                return False, "Пользователь не найден", None
+
+        # ошибка подключения
         except sqlite3.OperationalError as e:
-            return False, "Ошибка подклыючения к базе данных!"
+            return False, "Ошибка подклыючения к базе данных!", None
 
 
     # добавление аккаунта
-    def addition(self, first_name, surname, number_phone, password, replay_password):
+    def registration(self, first_name, surname, year_of_birth, number_phone, password):
         try:
             with sqlite3.connect(self.file_db) as conn:
                 cursor = conn.cursor()
-                cursor.execute(REGISTRATION, (first_name, surname, number_phone, password, replay_password))
-                return True, "Регистрация выполнена!"
+                cursor.execute(REGISTRATION, (first_name, surname, year_of_birth, number_phone, password))
+
+                cursor.execute(USER_PRESENCE, (number_phone, password))
+                data_user = cursor.fetchone()
+                return True, "Регистрация выполнена!", data_user
 
         # ошибка подключения
         except sqlite3.OperationalError as e:
-            return False, "Ошибка подклыючения к базе данных!"
+            return False, "Ошибка подключения к базе данных!", None
 
         # используется уже существующий номер телефона
         except sqlite3.IntegrityError as e:
-            return False, "Номер телефона уже занят!"
+            return False, "Этот номер телефона уже зарегистрирован!", None
 
-
-    # вход в аккаунт
-    def entrance(self, number_phone, password):
-        try:
-            with sqlite3.connect(self.file_db) as conn:
-                cursor = conn.cursor()
-                cursor.execute(ENTRANCE, (number_phone, password))
-                return True, "Успешный вход!"
-
-        # ошибка подключения
-        except sqlite3.OperationalError as e:
-            return None, "Ошибка подключения к базе данных!"
-
-    # изменение имени пользователя
-    def update_username(self, number_phone, new_name):
-        try:
-            with sqlite3.connect(self.file_db) as conn:
-                cursor = conn.cursor()
-                cursor.execute(UPDATE_USERNAME, (new_name, number_phone))
-                return True, "Имя успешно изменено!"
-
-        # ошибка подключения
-        except sqlite3.OperationalError as e:
-            return None, "Ошибка подключения к базе данных!"
 
     # изменение номера телефона
     def update_number_phone(self, number_phone, new_number_phone):
@@ -73,8 +55,8 @@ class DataBase:
 
         # ошибка подключения
         except sqlite3.OperationalError as e:
-            return None, "Ошибка подключения к базе данных!"
+            return False, "Ошибка подключения к базе данных!"
 
         # используется уже существующий номер телефона
         except sqlite3.IntegrityError as e:
-            return None, "Номер телефона уже занят!"
+            return False, "Номер телефона уже занят!"
