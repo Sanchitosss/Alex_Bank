3487d5bd8857d76c04947638927fc8c6bc48dc0c
Sanchitosss <Sashashurnov.777@gmail.com>
2026-03-03 21:10:39 +0300

    Update app.py

--- Алекс Банк/backend/app.py
+++ Алекс Банк/backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, flash
+from flask import Flask, render_template, redirect, url_for, request, flash, session
 from database.database import DataBase # работа с бд
 from security import AllValidations
 
@@ -6,17 +6,19 @@ from security import AllValidations
 app = Flask(__name__, template_folder='../frontend/templates',
                         static_folder='../frontend/static')
 
+app.secret_key = 'sanechek_brat_001'
+
 # объект на основе клааса для работы с бд
 db = DataBase('training.db')
 
 
-# главная страница
+# Главная страница
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-# вход в аккаунт
+# Вход в аккаунт
 @app.route('/authentication', methods=['POST', 'GET'])
 def auth():
     
@@ -31,11 +33,20 @@ def auth():
             return render_template('authentication.html')
         
         # результат и сообщение из функции, которая проверяет в бд наличие пользователя
-        success, message = db.user_presence(number_phone, password)
+        success, message, data_user = db.user_presence(number_phone, password)
 
         # если пользователь найден
         if success:
-            return render_template('account.html') # сделать переход в аккаунт!!!!!!!
+            # создаём сессию
+            session['id'] = data_user[0]
+            session['first_name'] = data_user[1]
+            session['surname'] = data_user[2]
+            session['year_of_birth'] = data_user[3]
+            session['number_phone'] = data_user[4]
+            session['balance'] = data_user[6]
+
+            return redirect(url_for('account'))
+
         # если пользователь не найден
         else:
             flash(message)
@@ -46,7 +57,7 @@ def auth():
 
 
 
-# регистрация аккаунта
+# Регистрация аккаунта
 @app.route('/registration', methods=['POST', 'GET'])
 def registration():
     if request.method == 'POST':
@@ -65,7 +76,7 @@ def registration():
 
 
         # если хотя бы одно поле не заполнено
-        fields = [first_name, surname, number_phone, password, replay_password]
+        fields = [first_name, surname, year_of_birth, number_phone, password, replay_password]
         if not all(fields):
             flash('Все поля должны быть заполнены!')
             return render_template('registration.html')
@@ -83,25 +94,64 @@ def registration():
         errors = valids.all_validations()
 
         # ищем ошибки в ведённых данных
-        has_errors = False
-        for key, error in errors.items():
-            if error:
-                has_errors = True
+        has_errors = any(error_list for error_list in errors.values())
+
         # если есть хотя бы одна ошибка
         if has_errors:
             for field_name, error_list in errors.items():
+                # выводим ошибки
                 for error in error_list:
                     flash(f'{field_name}: {error}')
             
             return render_template('registration.html')
 
-        # добавляем пользователя в бд, если проверки пройдены
-        # ДОБАВИТЬ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+        # далее добавляем пользователя в бд, если проверки пройдены
+        
+        # результаты и сообщение регистрации
+        success, message, data_user = db.registration(first_name=first_name, surname=surname, year_of_birth=year_of_birth, number_phone=number_phone, password=password)
+        
+        # если успешно зарегистрировались
+        if success:
+            # создаём сессию
+            session['id'] = data_user[0]
+            session['first_name'] = data_user[1]
+            session['surname'] = data_user[2]
+            session['year_of_birth'] = data_user[3]
+            session['number_phone'] = data_user[4]
+            session['balance'] = data_user[6]
+            
+            return redirect(url_for('account'))
+
+        # если ошибка добавления в бд
+        else:
+            flash(message)
+            return render_template('registration.html')
+        
     # при переходе на эту страницу
     return render_template('registration.html')
 
 
+# Аккаунт
+@app.route('/account', methods=['GET'])
+def account():
+    if 'id' not in session:
+        flash('Сначала войдите в аккаунт')
+        return redirect(url_for('auth'))
+    
+    return render_template('account.html',
+                          first_name=session['first_name'],
+                          surname=session['surname'],
+                          number_phone=session['number_phone'],
+                          balance=session['balance'])
+
+
+# Выход из аккаунта
+@app.route('/logout', methods=['POST'])
+def logout():
+    session.clear()  # Удаляем все данные сессии
+    flash('Вы вышли из аккаунта')
+    return redirect(url_for('index'))
 
 
 
